a675c40154d9b3b0c126ef435e3c575ef2643bdc
bnT91 <151850289+bnT91@users.noreply.github.com>
2024-12-11 20:31:53 +0300

    /create and /join handlers. Some /process code

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ bot = telebot.TeleBot('7892869505:AAEMltK8z1DRKhMjJ5HsU_IhTwGss7GDxZs')
 
 allowed_users = []
 game = False
+prep = False
+players = []
+creator = None
 
 with open("allowed_users.txt", "r") as f:
     for line in f:
@@ -26,10 +29,12 @@ def start(message):
                          reply_markup=markup)
 
     else:
-        if game:
-            print("Напишите /join, чтобы присоединиться к текущей игре. ")
+        if prep:
+            bot.send_message(message.chat.id, "🔗Напишите /join, чтобы присоединиться к текущей игре. ")
+        elif not game:
+            bot.send_message(message.chat.id, "На данный момент игра не запущена. ➕Напишите /create для создания новой.")
         else:
-            print("На данный момент игра не запущена. Напишите /create для создания новой.")
+            bot.send_message(message.chat.id, "Игра уже идёт. 🔚Пожалуйста, дождитесь её окончания и присоединитесь к новой / создайте её.")
 
 
 @bot.callback_query_handler()
@@ -41,6 +46,50 @@ def query_callback(callback):
         bot.send_message(int(callback.data[12:]), "🔓✅ Доступ разрешён!")
     elif callback.data[:12] == "decline_user":
         bot.send_message(int(callback.data[13:]), "❌Доступ запрещён.")
+
+
+@bot.message_handler(commands=["create"])
+def create(message):
+    global game, prep, players, creator
+    if prep:
+        bot.send_message(message.chat.id, "Игра уже создана. 🔗Чтобы присоединиться, напишите /join.")
+
+    elif not game:
+        bot.send_message(message.chat.id, "🎲🎮Вы создали новую игру. Сейчас участвуете только Вы. Каждый игрок, кто напишет /join, присоединится. "
+                                          "Когда Вы посчитаете, что набралось достаточное количество участников, напишите /process и игра начнётся. ")
+        creator = message.from_user.id
+        players.append(creator)
+        prep = True
+
+    else:
+        bot.send_message(message.chat.id, "Игра уже идёт. Пожалуйста, дождитесь её окончания и попробуйте ещё раз.")
+
+
+@bot.message_handler(commands=["join"])
+def join(message):
+    global players
+    if prep:
+        if message.from_user.id not in players:
+            bot.send_message(message.chat.id, "🔗Вы присоединились. Когда создатель игры напишет /process, она начнётся. ")
+            players.append(message.from_user.id)
+        else:
+            bot.send_message(message.chat.id, "Вы уже в игре. Ожидайте начала.")
+    elif not game:
+        bot.send_message(message.chat.id, "К сожалению, вы не можете присоединиться к уже начатой игре. 🔚Пожалуйста, дождитесь её конца.")
+    else:
+        bot.send_message(message.chat.id, "В данный момент игры не существует. Чтобы создать новую, напишите /create.")
+
+
+@bot.message_handler(commands=["process"])
+def process(message):
+    global prep, game
+    if prep:
+        if message.from_user.id == creator:
+            game = True
+            prep = False
+            bot.send_message(message.chat.id, "Игра началась! Для её остановки напишите /abort.")
+        else:
+            bot.send_message(message.chat.id, "Не Вы создали данную игру. 🕔Ожидайте админа для старта.")
 
 
 bot.polling(non_stop=True)
